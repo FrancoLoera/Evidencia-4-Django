864e2a30a16557bb910af4f991fe05821a2b7cdc
FrancoLoera <theprivate_cloud@hotmail.com>
2025-10-30 10:41:21 -0600

    Nueva tabla Categoría Migrada. Pendiente diseño, regex e interlinkeado

--- proyecto_prueba/app/models.py
+++ proyecto_prueba/app/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Categoria (models.Model):
+    nombre = models.CharField(max_length = 100)
+
+    def __str__(self):
+        return self.nombre
+
 class Producto (models.Model):
     nombre = models.CharField(max_length = 100)
     precio = models.DecimalField(max_digits = 10,  decimal_places = 2)
     descripcion = models.TextField(max_length = 250, null = True)
 
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, null = True)
+    
     def __str__(self):
         return self.nombre
