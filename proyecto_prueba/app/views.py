2d5439311bf8727a05bdd589eee29258d2db3575
FrancoLoera <theprivate_cloud@hotmail.com>
2025-10-30 17:53:12 -0600

    Formulario de Creación de productos terminado. Pendiente: Formulario de Editar productos y Formularios de Categorías

--- proyecto_prueba/app/views.py
+++ proyecto_prueba/app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Producto
+from .models import Producto, Categoria
 
 def listar_productos(request):
     productos = Producto.objects.all()
@@ -13,9 +13,14 @@ def crear_producto(request):
         nombre = request.POST['nombre']
         precio = request.POST['precio']
         descripcion = request.POST['descripcion']
-        Producto.objects.create(nombre=nombre, precio=precio, descripcion=descripcion)
+        categoria_id = request.POST.get('categoria')
+
+        categoria = Categoria.objects.get(id=categoria_id)
+
+        Producto.objects.create(nombre=nombre, precio=precio, descripcion=descripcion, categoria=categoria)
         return redirect('listar')
-    return render(request, 'crear.html')
+    categorias = Categoria.objects.all()
+    return render(request, 'crear.html', {'categorias': categorias})
 
 def editar_producto(request, id):
     producto = get_object_or_404(Producto, id=id)
