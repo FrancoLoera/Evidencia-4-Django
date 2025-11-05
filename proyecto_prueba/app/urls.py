16d776477964bdb215ff478d1937ba9e3e258666
FrancoLoera <theprivate_cloud@hotmail.com>
2025-11-04 22:08:09 -0600

    Sistema terminado. Pendiente a posibles cambios

--- proyecto_prueba/app/urls.py
+++ proyecto_prueba/app/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('crear/', views.crear_producto, name='crear'),
     path('editar/<int:id>/', views.editar_producto, name='editar'),
     path('eliminar/<int:id>/', views.eliminar_producto, name='eliminar'),
+    path('listar-categorias/', views.listar_categorias, name='listar-categorias'),
+    path('crear-categoria/', views.crear_categoria, name="crear-categoria"),
+    path('editar-categoria/<int:id>/', views.editar_categoria, name="editar-categoria"),
+    path('eliminar-categoria/<int:id>/', views.eliminar_categoria, name="eliminar-categoria"),
 ]
--- proyecto_prueba/app/views.py
+++ proyecto_prueba/app/views.py
@@ -1,5 +1,3 @@
-from django.shortcuts import render
-
 # Create your views here.
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Producto, Categoria
@@ -43,3 +41,31 @@ def eliminar_producto(request, id):
     producto = get_object_or_404(Producto, id=id)
     producto.delete()
     return redirect('listar')
+
+def listar_categorias(request):
+    categorias = Categoria.objects.all()
+    return render(request, 'listar-categorias.html', {'categorias': categorias})
+
+def crear_categoria(request):
+    if request.method == 'POST':
+        nombre = request.POST['nombre']
+        
+        Categoria.objects.create(nombre = nombre)
+        return redirect('listar-categorias')
+
+    return render(request, 'crear-categoria.html')
+
+def editar_categoria(request, id):
+    categoria = get_object_or_404(Categoria, id=id)
+    if request.method == 'POST':
+        categoria.nombre = request.POST['nombre']
+
+        categoria.save()
+        return redirect('listar-categorias')
+    
+    return render(request, 'editar-categoria.html', {'categoria': categoria})
+
+def eliminar_categoria(request, id):
+    categoria = get_object_or_404(Categoria, id=id)
+    categoria.delete()
+    return redirect('listar-categorias')
