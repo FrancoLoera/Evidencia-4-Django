42fb6bb97ce12fbad5e286d0d7a325234f17dee4
FrancoLoera <theprivate_cloud@hotmail.com>
2025-11-05 23:06:09 -0600

    Página de Creación de contacto en desarrollo

--- agenda_telefonica/agenda/urls.py
+++ agenda_telefonica/agenda/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.listar_contactos, name='listar-contactos'),
+    path('crear-contacto/', views.crear_contacto, name='crear-contacto'),
 ]
--- agenda_telefonica/agenda/views.py
+++ agenda_telefonica/agenda/views.py
@@ -5,3 +5,22 @@ from .models import Relacion, Contacto
 def listar_contactos(request):
     contactos = Contacto.objects.all()
     return render(request, 'listar-contactos.html', {'contactos': contactos})
+
+def crear_contacto(request):
+    if request.method == "POST":
+        nombre = request.POST["nombre"]
+        apellidoPaterno = request.POST["apellidoPaterno"]
+        apellidoMaterno = request.POST.get("apellidoMaterno", "")
+        alias = request.POST.get("alias", "")
+        
+        relacion_id = request.POST.get("relacion")
+        relacion = Relacion.objects.get(id = relacion_id)
+        
+        telefono = request.POST["telefono"]
+        correo = request.POST.get("correo", "")
+        
+        Contacto.objects.create(nombre = nombre, apellidoPaterno = apellidoPaterno, apellidoMaterno = apellidoMaterno, alias = alias, relacion = relacion, telefono = telefono, correo = correo)
+        return redirect("listar-contactos")
+    
+    relaciones = Relacion.objects.all()
+    return render(request, "crear-contacto.html", {"relaciones": relaciones})
