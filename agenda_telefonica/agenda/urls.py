db3318cfaeca071eb9ea453602763e4abeaeadbc
FrancoLoera <theprivate_cloud@hotmail.com>
2025-11-06 09:19:57 -0600

    Módulos terminados

--- agenda_telefonica/agenda/urls.py
+++ agenda_telefonica/agenda/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.listar_contactos, name='listar-contactos'),
     path('crear-contacto/', views.crear_contacto, name='crear-contacto'),
+    path('editar-contacto/<int:id>/', views.editar_contacto, name='editar-contacto'),
+    path('eliminar-contacto/<int:id>/', views.eliminar_contacto, name='eliminar-contacto'),
 ]
--- agenda_telefonica/agenda/views.py
+++ agenda_telefonica/agenda/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
 from .models import Relacion, Contacto
 
 # Create your views here.
@@ -18,9 +19,40 @@ def crear_contacto(request):
         
         telefono = request.POST["telefono"]
         correo = request.POST.get("correo", "")
-        
-        Contacto.objects.create(nombre = nombre, apellidoPaterno = apellidoPaterno, apellidoMaterno = apellidoMaterno, alias = alias, relacion = relacion, telefono = telefono, correo = correo)
-        return redirect("listar-contactos")
+
+        if Contacto.objects.filter(telefono = telefono).exists():
+            messages.error(request, "El teléfono ya está registrado.")
+
+        elif Contacto.objects.filter(correo = correo).exists():
+            messages.error(request, "El correo ya está registrado.")
+
+        else:
+            Contacto.objects.create(nombre = nombre, primerApellido = apellidoPaterno, segundoApellido = apellidoMaterno, alias = alias, relacion = relacion, telefono = telefono, correo = correo)
+            return redirect("listar-contactos")
     
     relaciones = Relacion.objects.all()
     return render(request, "crear-contacto.html", {"relaciones": relaciones})
+
+def editar_contacto(request, id):
+    contacto = get_object_or_404(Contacto, id=id)
+    if request.method == 'POST':
+        contacto.nombre = request.POST["nombre"]
+        contacto.primerApellido = request.POST["apellidoPaterno"]
+        contacto.segundoApellido = request.POST["apellidoMaterno"]
+        contacto.alias = request.POST["alias"]
+        contacto.telefono = request.POST["telefono"]
+        contacto.correo = request.POST["correo"]
+
+        relacion_id = request.POST["relacion"]
+        if relacion_id:
+            contacto.relacion = Relacion.objects.get(id = relacion_id)
+
+        contacto.save()
+        return redirect('listar-contactos')
+    relaciones = Relacion.objects.all()
+    return render(request, 'editar-contacto.html', {'contacto': contacto, 'relaciones': relaciones})
+
+def eliminar_contacto(request, id):
+    contacto = get_object_or_404(Contacto, id=id)
+    contacto.delete()
+    return redirect('listar-contactos')
